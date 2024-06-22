394a8ac99caa16f5ed966e9e7fb0d68e5fa03cbc
Ulo Eruma-Gborie <uloerumagborie@gmail.com>
2024-06-22 18:54:23 +0000

    changed debug to false

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-uloeg-djangoblog-71tez4q5l0i.ws.codeinstitute-ide.net',
                 '.herokuapp.com']
